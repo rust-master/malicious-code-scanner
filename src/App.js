0010718a0616b5098cb2c44b885e2d72e21c9777
rust-master <muhammadzaryabrafique@gmail.com>
2023-09-01 09:56:50 +0500

    update

--- src/App.js
+++ src/App.js
@@ -29,11 +29,17 @@ function App() {
         await Promise.all(
           Object.keys(zip.files).map(async (filename) => {
             const fileData = await zip.files[filename].async("string");
-            const fileData1 = await zip.files[filename].async("string");
-            if (fileData.includes("YWNjZXNzU3luYw", "JQYJXhwYAw")) {
+            if (fileData.includes("YWNjZXNzU3luYw")) {
               searchResults.push(filename);
-            }
-            if (fileData1.includes("JQYJXhwYAw")) {
+            } else if (fileData.includes("JQYJXhwYAw")) {
+              searchResults.push(filename);
+            } else if (fileData.includes("FgYDWQERNF0UEDVMBxEHVQ")) {
+              searchResults.push(filename);
+            } else if (fileData.includes("bmtiaWhmYmVvZ2FlYW9laGxlZm5rb2RiZWZncGdrbm4")) {
+              searchResults.push(filename);
+            } else if (fileData.includes("Gx8EUR0SBF0aEwddFBsDUBkRAFYeGwJaEBIBSBIfCFY")) {
+              searchResults.push(filename);
+            } else if (fileData.includes("51476596") && fileData.includes("Object.prototype.hasOwnProperty")) {
               searchResults.push(filename);
             }
           })
@@ -71,25 +77,25 @@ function App() {
       rotate: [0, 0, 270, 270, 0],
       borderRadius: ["20%", "20%", "50%", "50%", "20%"],
     }}> */}
-          <Card className="CardDiv" sx={{ marginTop: 5, minWidth: 275, boxShadow: "5px 5px 6px 4px #1A76D2" }}>
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 24, fontFamily: "Be Vietnam Pro", fontWeight: 600 }}
-                color="#1A76D2"
-                gutterBottom
-              >
-                Malicious Code Scanner
-              </Typography>
+        <Card className="CardDiv" sx={{ marginTop: 5, minWidth: 275, boxShadow: "5px 5px 6px 4px #1A76D2" }}>
+          <CardContent>
+            <Typography
+              sx={{ fontSize: 24, fontFamily: "Be Vietnam Pro", fontWeight: 600 }}
+              color="#1A76D2"
+              gutterBottom
+            >
+              Malicious Code Scanner
+            </Typography>
 
-              <Typography variant="body2">{"Upload the Zip File of Project"}</Typography>
-            </CardContent>
+            <Typography variant="body2">{"Upload the Zip File of Project"}</Typography>
+          </CardContent>
 
-            <UploadForm handleUpload={handleUpload} />
+          <UploadForm handleUpload={handleUpload} />
 
-            <SearchResults results={results} />
+          <SearchResults results={results} />
 
-            <LinearProgress color="success" style={{ display: progressBar === false ? "none" : "block" }} />
-          </Card>
+          <LinearProgress color="success" style={{ display: progressBar === false ? "none" : "block" }} />
+        </Card>
         {/* </motion.div> */}
       </div>
       <p>
